df30b904960685205e524f152248a26da2ac38b2
axel paez <paezaxel2221@gmail.com>
2023-10-13 20:38:16 -0600

    logis, registrar, recuperar contraseña agregados

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { ReporteServices } from 'src/app/services/reportes.services';
+
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-home',
@@ -8,9 +15,58 @@ import { GoogleMap } from '@angular/google-maps';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public mostrar:boolean;
+  public eventos:any[];
+
+  constructor(
+    private _fb: FormBuilder,
+    private _rServices: ReporteServices,
+    private _router: Router,
+    private _route: ActivatedRoute, 
+  ) { 
+    this.mostrar = true;
+    this.eventos = [];
+  }
 
   ngOnInit(): void {
+    this.getReportes();
+    this.getEventos();
+  }
+
+  getReportes(){
+   this._rServices.getIncidentes().subscribe( response => {
+      console.log(response)
+    }, error => {
+      console.log('Error ', error)
+      Swal.fire({
+        icon: 'warning',
+        title: '¡Oops!',
+        text: 'Ocurrio un error al intentar actualizar la lista de incidentes',
+        showConfirmButton: true,
+        heightAuto: false
+      });
+
+    })
+  }
+
+  getEventos(){
+    this._rServices.getEventos().subscribe( response => {
+      this.eventos = response.Eventos;
+      console.log(this.eventos)
+    }, error => {
+      console.log('Error ', error)
+      Swal.fire({
+        icon: 'warning',
+        title: '¡Oops!',
+        text: 'Ocurrio un error al intentar actualizar la lista de eventos',
+        showConfirmButton: true,
+        heightAuto: false
+      });
+
+    })
+  }
+  showHidden(){
+    this.mostrar = ( this.mostrar ) ? false: true; 
   }
 
 }
--- src/app/components/request-pass/request-pass.component.ts
+++ src/app/components/request-pass/request-pass.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import Swal from 'sweetalert2';
+
+import { UserServices } from 'src/app/services/user.services';
 
 @Component({
   selector: 'app-request-pass',
@@ -7,9 +13,120 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestPassComponent implements OnInit {
 
-  constructor() { }
+  validForm!: FormGroup;
+  public item:number;
+  public spinner: boolean;
+  private token: string;
+
+  constructor(
+    private _fb: FormBuilder,
+    private _usrServices: UserServices,
+    private _router: Router,
+    private _route: ActivatedRoute, 
+  ) { 
+    this.item = 1;
+    this.spinner = false;
+    this.token = '';
+  }
+
+  get Password(){
+    return this.validForm.get("Password");
+  }
+  get RePassword(){
+    return this.validForm.get("RePassword");
+  }
 
   ngOnInit(): void {
+    this.validForm = this.initForm();
+    this.getToken();
   }
+
+  getToken(){
+    this._route.params.subscribe( params =>{
+      const token = params['token'];
+      if(token){
+        this.token = token;
+        console.log(this.token);
+      }else {
+        Swal.fire({
+          icon: 'error',
+          title: '¡Oops!',
+          text: 'No se pudo obtener el token',
+          showConfirmButton: true,
+          heightAuto: false
+        });
+        this._router.navigate(['/Auth']);
+      }
+    })
+  }
+
+  initForm():FormGroup {
+    return this._fb.group({
+      Password: ['',[ Validators.required,
+        Validators.pattern(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[A-Z]).{8,}$/)]],
+      RePassword: [ '', [Validators.required,
+           this.confirmPass.bind(this)]],
+    })}
+
+    confirmPass(control:any){
+      return (  this.validForm?.get('Password')?.value == control.value ) ? null : { noCoincide: true }
+    }
+
+    errorsInput(e:any, tipo:string){
+      console.log(e)
+      if(e?.required){
+        return `${tipo} requerido` ;
+      }else if(e?.noCoincide){
+        return `Las contraseñas no son iguales`; 
+      }else{
+        return `${tipo} no valido`; 
+      }
+    }
+
+    onSubmit(e:any){
+      this.spinner = true;
+      console.log("Usuarui ", this.validForm.value )
+      if( !this.validForm.invalid ){
+
+        const data = {
+          "Password": this.validForm.value.Password,
+          "Token": this.token
+        }
+
+        console.log("token ", data);
+        this._usrServices.RequestPassword(data).subscribe( response => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Se ha cambiado correctamente la contraseña',
+            showConfirmButton: true,
+            heightAuto: false,
+          });
+          console.log("response ", response );
+          this._router.navigate(['/Auth']);
+
+        }, error => {
+          console.log(error);
+          this.spinner = false;
+          let errorMensaje = ( error?.error?.Mensaje ) ? error?.error?.Mensaje : 'Ocurrio un error al intentar cambiar la contraseña';
+          console.log("error ", error);
+          Swal.fire({
+            icon: 'warning',
+            title: '¡Oops!',
+            text: errorMensaje,
+            showConfirmButton: true,
+            heightAuto: false
+          });
+        })
+      }else{
+      this.spinner = false;
+      Swal.fire({
+        icon: 'warning',
+        title: '¡Oops!',
+        text: 'Parece que el formulario no esta correctamente lleno, por favor reviselo e intentelo de nuevo',
+        showConfirmButton: true,
+        heightAuto: false
+      });
+    }
+    }
 
 }
